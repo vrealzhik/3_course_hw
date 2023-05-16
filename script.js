46648205cc9700514884630c44f20583b762edf8
Vladislav <vzhalin81@gmail.com>
2023-05-16 13:36:27 +0300

    Event

---
+++ script.js
@@ -0,0 +1,19 @@
+const radioChoiceElemenet = document.querySelectorAll(`input[type=radio][name="diff"]`);
+const startElement = document.getElementById("startBtn");
+
+export let selectedDiff = {
+    diff: 0,
+};
+
+radioChoiceElemenet.forEach(radio => radio.addEventListener("change", () => {
+    selectedDiff.diff = radio.value
+}))
+
+startElement.addEventListener("click", () =>{
+    if(!selectedDiff) {
+        alert("Выберите сложность!");
+        return;
+    }
+    console.log (selectedDiff.diff);
+    //window.location.href = 'test-relocate.html'
+})
---
+++ test-relocte.js
@@ -0,0 +1,15 @@
+// import { selectedDiff } from "./script.js";
+
+// const renderApp = () => {
+//     const appEl = document.getElementById("app");
+
+//     const appHtml = `
+//     <h1>Выбранная сложность:</h1>
+//     <p>${selectedDiff}</p>
+//     <a href="index.html">Вернуться к выбору сложности</a>
+//     `
+
+//     appEl.innerHTML = appHtml
+// }
+
+// renderApp();
