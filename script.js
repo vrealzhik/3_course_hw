60fed6d251b8a0c39afd03ecd430f84c7a63d33f
Vladislav <vzhalin81@gmail.com>
2023-06-11 14:59:01 +0300

    added pckgs

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const radioChoiceElemenet = document.querySelectorAll(`input[type=radio][name="diff"]`);
-const startElement = document.getElementById("startBtn");
+const radioChoiceElemenet = document.querySelectorAll(
+    `input[type=radio][name="diff"]`
+);
+const startElement = document.getElementById('startBtn');
 
-export let selectedDiff = {
+let selectedDiff = {
     diff: 0,
 };
 
-radioChoiceElemenet.forEach(radio => radio.addEventListener("change", () => {
-    selectedDiff.diff = radio.value
-}))
+radioChoiceElemenet.forEach((radio) =>
+    radio.addEventListener('change', () => {
+        selectedDiff.diff = radio.value;
+    })
+);
 
-startElement.addEventListener("click", () =>{
-    if(!selectedDiff) {
-        alert("Выберите сложность!");
+startElement.addEventListener('click', () => {
+    if (!selectedDiff) {
+        alert('Выберите сложность!');
         return;
     }
-    console.log (selectedDiff.diff);
-    window.location.href = 'second_page.html'
-})
+    console.log(selectedDiff.diff);
+    window.location.href = 'second_page.html';
+});
